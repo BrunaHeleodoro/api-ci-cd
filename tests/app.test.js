128ada041db88687b6f5c141492124a34b12c24d
Bruna Heleodoro <bruna.heleodoro@funcionalcorp.com.br>
2025-04-25 16:03:04 -0300

    test: ajusta teste de listagem de usuários

--- tests/app.test.js
+++ tests/app.test.js
@@ -14,10 +14,11 @@ describe('GET /users', () => {
         const response = await request(app).get ('/users');
         expect(response.status).toBe(200);
         expect(Array.isArray(response.body)).toBe(true);
-        expect(response.body.length).toBe(2);
+        expect(response.body.length).toBe(3);
         
         expect(response.body[0]).toMatchObject({ id: 1, name: 'Bruna' });
         expect(response.body[1]).toMatchObject({ id: 2, name: 'Aura' });
+        expect(response.body[2]).toMatchObject({ id: 3, name: 'Marina' },);
     })
 })
 
